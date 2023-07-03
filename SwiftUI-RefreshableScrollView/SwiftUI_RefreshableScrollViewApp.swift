9552bea92ba5d2004e00499b55fe40bd68c647ab
Vanson Leung <vanson@vanportdev.com>
2023-07-03 20:27:09 +0800

    Initial Commit

---
+++ SwiftUI-RefreshableScrollView/SwiftUI_RefreshableScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RefreshableScrollViewApp.swift
+//  SwiftUI-RefreshableScrollView
+//
+//  Created by Leung Yu Wing on 3/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RefreshableScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
